92d22fc51ff8b1734f633f28f96af6f4765ddb4b
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2021-04-15 18:28:20 +0200

    second commit

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const port = 8888;
 const fs=require('fs');
 
 
-const server =http.createServer((req,res)=>{ 
+const server =http.createServer((req,res)=>{
   if(req.method=="GET"&&req.url=="/"){
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('index.html').pipe(res)
-  }else{
+}else if (req.method=="GET"&&req.url=="/lavdim") {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index1.html').pipe(res)
+}
+
+  else{
 	 res.writeHead(200, { 'content-type': 'text/plain' })
 	 res.end("Error")
   }
